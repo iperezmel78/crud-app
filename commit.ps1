41a5afb80c3db385c5b47284aa2811adf449dc4e
Israel <iperezmel78@gmail.com>
2022-11-29 14:21:33 -0500

    for jenkins test

---
+++ commit.ps1
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "for jenkins test"
+git push origin master
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,6 @@ export const environment = {
   apiURL: 'http://localhost:8080/api'
 };
 
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
