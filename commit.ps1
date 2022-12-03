20446c5d6d3fb6764e886a90319e67685710ff45
Israel <iperezmel78@gmail.com>
2022-12-02 20:09:37 -0500

    added ng build memory configuration

--- commit.ps1
+++ commit.ps1
@@ -1,3 +1,8 @@
-git add -A
-git commit -m "for jenkins test"
-git push origin master
+If (!$args[0]) {
+  Write-Host "A message is needed"
+} else {
+  git status
+  git add -A
+  git commit -m $args[0]
+  git push
+}
